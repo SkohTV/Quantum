d6f427a5dbedb71e61f91f77ed06dbe1aaa4838e
SkohTV <qlpth+github@proton.me>
2025-02-16 16:08:02 +0100

    fixed bug in /clusterupdate

--- src/consts.rs
+++ src/consts.rs
@@ -1,12 +1,9 @@
 use const_format::concatcp;
 
-
-
 const MAJOR: u8 = 5;
-const MINOR: u8 = 3;
+const MINOR: u8 = 4;
 
 pub const MODE: &str = "dev";
 // pub const MODE: &str = "release";
-
 
 pub const VERSION: &str = concatcp!(MAJOR, ".", MINOR, "-", MODE);
--- src/discord/commands/clusterupdate.rs
+++ src/discord/commands/clusterupdate.rs
@@ -1,42 +1,30 @@
-use poise::serenity_prelude as serenity;
-use crate::discord::{Context, Error, ids};
 use crate::database;
+use crate::discord::{ids, Context, Error};
+use poise::serenity_prelude as serenity;
 use std::str::FromStr;
-
-
 
 /// Remove and add back all cluster roles, for a refresh in case of bugs
 #[poise::command(
     slash_command,
-    rename="clusterupdate",
-    default_member_permissions="ADMINISTRATOR",
+    rename = "clusterupdate",
+    default_member_permissions = "ADMINISTRATOR"
 )]
-pub async fn cmd(
-    ctx: Context<'_>,
-) -> Result<(), Error> {
-
+pub async fn cmd(ctx: Context<'_>) -> Result<(), Error> {
     let text = format!("⌛ Loading...");
     let response = ctx.say(text).await?;
 
+    // Fetch all users & db
     let cluster_role = serenity::RoleId::from(ids::CLUSTER_ROLE);
     let guild = ctx.guild().unwrap().clone();
 
     let con = database::client::start_db().await;
     let mut cluster_members = database::requests::retrieve_users(con).await.unwrap();
 
-    while let Some(user) = cluster_members.next().await? {
-        let uid: String = user.get(0)?;
-        let uid = serenity::UserId::from_str(uid.as_str()).unwrap();
-
-        if let Ok(member) = guild.member(ctx, uid).await {
-            member.add_role(ctx, cluster_role).await?;
-        }
-    }
-
+    // Remove all cluster roles
     let server_members = guild.members(ctx, None, None).await?;
-
     for user in &server_members {
-        let roles = user.roles(ctx)
+        let roles = user
+            .roles(ctx)
             .unwrap()
             .iter()
             .map(|x| x.id)
@@ -47,11 +35,21 @@ pub async fn cmd(
         }
     }
 
-    let msg = poise::CreateReply::default()
-        .content(format!("👍 All roles for users in cluster have been refreshed"));
+    // Add back all cluster roles
+    while let Some(user) = cluster_members.next().await? {
+        let uid: String = user.get(0)?;
+        let uid = serenity::UserId::from_str(uid.as_str()).unwrap();
+
+        if let Ok(member) = guild.member(ctx, uid).await {
+            member.add_role(ctx, cluster_role).await?;
+        }
+    }
+
+    let msg = poise::CreateReply::default().content(format!(
+        "👍 All roles for users in cluster have been refreshed"
+    ));
 
     response.edit(ctx, msg).await?;
-
 
     Ok(())
 }
